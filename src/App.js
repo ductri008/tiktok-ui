7704c8fd10e7f21ad5f56d3cda1a3180ed1b100c
Your Name <you@example.com>
2022-08-23 14:55:41 +0700

    TIKTOK BASE

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    GITHUB CUA DUCTRI008
-                </a>
-                <button>TEST BUTTON</button>
-                <button>TEST BUTTON2</button>
-                <button>TEST BUTTON3</button>
-                <button>TEST BUTTON4</button>
+                <h1> REACT APP</h1>
             </header>
         </div>
     );
