78aa3171775cbb2d8cf29921e033a7a013f84409
Your Name <you@example.com>
2022-08-23 11:25:00 +0700

    asd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GITHUB CUA DUCTRI008
         </a>
       </header>
     </div>
